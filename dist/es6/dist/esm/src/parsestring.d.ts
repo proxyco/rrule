da8b4a8977f324c6fd329c8958416805aa9a7fd4
Simon Ratner <simon+github@probablyprime.net>
2019-08-16 18:26:17 -0700

    Commit es6 dist files to make module usable from git

---
+++ dist/es6/dist/esm/src/parsestring.d.ts
@@ -0,0 +1,4 @@
+import { Options, DateTimeProperty } from './types';
+export declare function parseString(rfcString: string): Partial<Options>;
+export declare function parseDateTime(line: string, prop?: DateTimeProperty): Partial<Options>;
+//# sourceMappingURL=parsestring.d.ts.map
